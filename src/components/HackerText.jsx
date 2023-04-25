cb66782a03c00d8fda2fdda8b381d68b77e7012a
jerryhuangyu <jerry0932619628@gmail.com>
2023-04-26 01:28:26 +0800

    update hacker text in react way

--- src/components/HackerText.jsx
+++ src/components/HackerText.jsx
@@ -1,41 +1,48 @@
-import { useEffect } from 'react'
+import { useEffect, useRef, useCallback } from 'react'
 import { styles } from '../styles';
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let interval = null;
 
 const HackerText = () => {
+  const hackerTextRef = useRef(null);
+
+  const handleMouseOver = useCallback((e) => {
+    let iteration = 0;
+    
+    clearInterval(interval);
+    interval = setInterval(() => {
+      e.target.innerText = e.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if(index < iteration) {
+            return e.target.dataset.value[index];
+          }
+        
+          return letters[Math.floor(Math.random() * letters.length)]
+        })
+        .join("");
+      
+      if(iteration >= e.target.dataset.value.length){ 
+        clearInterval(interval);
+      }
+      
+      iteration += 1 / 5;
+    }, 50);
+  }, [])
+
   useEffect(() => {
-    document.querySelector("#hacker_text").addEventListener("mouseover", (e) => {
-      let iteration = 0;
-      
-      clearInterval(interval);
-      
-      interval = setInterval(() => {
-        console.log(e.target.value)
-        e.target.innerText = e.target.innerText
-          .split("")
-          .map((letter, index) => {
-            if(index < iteration) {
-              return e.target.dataset.value[index];
-            }
-          
-            return letters[Math.floor(Math.random() * 26)]
-          })
-          .join("");
-        
-        if(iteration >= e.target.dataset.value.length){ 
-          clearInterval(interval);
-        }
-        
-        iteration += 1 / 5;
-      }, 50);
-    })
-  })
+    const node = hackerTextRef.current;
+    node.addEventListener("mouseover", handleMouseOver);
+    return () => {
+      node.removeEventListener("mouseover", handleMouseOver);
+      clearInterval(intervalRef.current);
+    };
+  }, [handleMouseOver]);
 
   return (
     <h1 className={`${styles.heroHeadText} text-secondary`}>
-      Hi, I'm <span id='hacker_text' data-value='Jerry' className='text-blue-out'>Jerry</span>
+      Hi, I'm <span ref={hackerTextRef} data-value='Jerry' className='text-blue-out'>Jerry</span>
     </h1>
   )
 }
