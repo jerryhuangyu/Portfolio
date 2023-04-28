7fdd34b6526bc3fd902df5e95c89bce645c9f2be
jerryhuangyu <jerry0932619628@gmail.com>
2023-04-29 02:00:48 +0800

    animate cursor blob shape

--- src/components/CursorBlob.jsx
+++ src/components/CursorBlob.jsx
@@ -1,12 +1,15 @@
 import { useRef, useEffect } from "react";
 
 const CursorBlob = () => {
-  const positionRef = useRef({
-    mouseX: 0,
-    mouseY: 0,
-  })
+  const blob = useRef(null);
 
   useEffect(() => {
+    // set interval for update blob border shape
+    setInterval(() => {
+      blob.current.style.borderRadius = `39% 61% 46% 54% / ${Math.floor(Math.random() * 50)}% ${Math.floor(Math.random() * 50)}% ${Math.floor(Math.random() * 50)}% ${Math.floor(Math.random() * 50)}%`;
+    }, 1000);
+
+    // when mousemove update blob's position to mouse
     document.addEventListener("mousemove", (e) => {
       const { pageX, pageY } = e;
       cursor_blob.animate(
@@ -14,17 +17,33 @@ const CursorBlob = () => {
           left: `${pageX}px`,
           top: `${pageY}px`,
         }, { 
+          duration: 1000,
+          fill: 'forwards'
+        }
+      );
+    });
+
+    // when scroll update blob's y position with scroll
+    document.addEventListener("scroll", (e) => {
+      // const { scrollY } = e;
+      cursor_blob.animate(
+        {
+          top: `${window.scrollY+360}px`,
+        }, { 
           duration: 7000,
           fill: 'forwards'
         }
       );
     });
+    
   });
 
   return (
-    <div 
+    <div
+      ref={blob}
       id='cursor_blob'
-      className="absolute blob-bg-gradient w-[400px] aspect-square translate-x-[-50%] translate-y-[-50%] rounded-full"
+      className="absolute blob-bg-gradient w-[400px] aspect-square translate-x-[-50%] translate-y-[-50%] rounded-full duration-1000 ease-in-out"
+      style={{ borderRadius: "49% 51% 70% 30% / 67% 68% 32% 33%"}}
     >
       
     </div>
