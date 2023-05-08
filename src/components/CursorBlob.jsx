ed5287fb9578cf08d701dc6da76e43b56b5dfcd8
jerryhuangyu <jerry0932619628@gmail.com>
2023-05-08 15:14:23 +0800

    fix z indxe

--- src/components/CursorBlob.jsx
+++ src/components/CursorBlob.jsx
@@ -48,14 +48,12 @@ const CursorBlob = () => {
     <>
       <div
         className="bg-black bg-opacity-[.15] blur-xl backdrop-blur-3xl
-        fixed top-0 w-screen h-screen z-20"
+        fixed top-0 w-screen h-screen z-10"
       ></div>
       <div
         ref={blob}
         id="cursor_blob"
-        className="absolute blob-bg-gradient opacity-50 w-[500px] aspect-square
-        translate-x-[-50%] translate-y-[-50%] rounded-full duration-[2000ms] ease-linear
-        z-10"
+        className="absolute blob-bg-gradient opacity-50 w-[500px] aspect-square translate-x-[-50%] translate-y-[-50%] rounded-full duration-[2000ms] ease-linear z-0"
         style={{ borderRadius: "49% 51% 70% 30% / 67% 68% 32% 33%", opacity: "50%" }}
       ></div>
     </>
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -124,7 +124,7 @@ const Works = ({ setSelectedPage }) => {
       initial="hidden"
       whileInView="show"
       viewport={{ once: true, amount: 0.25 }}
-      className={`${styles.padding} max-w-7xl mx-auto relative`}
+      className={`${styles.padding} max-w-7xl mx-auto relative z-40`}
     >
       <span className="hash-span" id="work">
         &nbsp;
