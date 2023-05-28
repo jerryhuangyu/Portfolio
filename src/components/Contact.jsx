0cb50ab1aceb20cdc766a31ae3546e2cbf62913b
jerryhuangyu <jerry0932619628@gmail.com>
2023-05-28 14:14:05 +0800

    prettier

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,8 +10,8 @@ import { staggerContainer } from "../utils/motion";
 import { Pages } from "../constants";
 
 const InputField = ({ type, name, value, handleChange, placeholderText }) => (
-  <div className="mt-2">
-    <div className="relative w-full">
+  <div className='mt-2'>
+    <div className='relative w-full'>
       <input
         required
         id={`form${name}`}
@@ -19,12 +19,13 @@ const InputField = ({ type, name, value, handleChange, placeholderText }) => (
         name={name}
         value={value}
         onChange={handleChange}
-        className="peer h-full w-full resize-none rounded-[7px] border-2 border-gray-50 bg-transparent px-6 py-4 text-sm font-normal text-gray-500 outline outline-0 transition-all placeholder-shown:border-gray-100 placeholder-shown:border-t-gray-200 focus:border-pink-500 focus:border-t-transparent focus:outline-0 dark:border-gray-400 dark:placeholder-shown:border-gray-400 dark:focus:border-pink-500 dark:focus:border-t-transparent"
-        placeholder=" "
+        className='peer h-full w-full resize-none rounded-[7px] border-2 border-gray-50 bg-transparent px-6 py-4 text-sm font-normal text-gray-500 outline outline-0 transition-all placeholder-shown:border-gray-100 placeholder-shown:border-t-gray-200 focus:border-pink-500 focus:border-t-transparent focus:outline-0 dark:border-gray-400 dark:placeholder-shown:border-gray-400 dark:focus:border-pink-500 dark:focus:border-t-transparent'
+        placeholder=' '
       ></input>
       <label
-      htmlFor={`form${name}`}
-      className="before:content[' '] after:content[' '] text-blue-gray-400 pointer-events-none absolute left-0 -top-[7px] flex h-full w-full select-none text-[11px] font-normal leading-tight transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-transparent before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-transparent after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-gray-500">
+        htmlFor={`form${name}`}
+        className="before:content[' '] after:content[' '] text-blue-gray-400 pointer-events-none absolute left-0 -top-[7px] flex h-full w-full select-none text-[11px] font-normal leading-tight transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-transparent before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-transparent after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-gray-500"
+      >
         {value ? "" : placeholderText}
       </label>
     </div>
@@ -32,14 +33,14 @@ const InputField = ({ type, name, value, handleChange, placeholderText }) => (
 );
 
 const InputTextarea = ({ name, value, handleChange, placeholderText }) => (
-  <div className="mt-2">
-    <div className="relative w-full">
+  <div className='mt-2'>
+    <div className='relative w-full'>
       <textarea
         name={name}
         value={value}
         onChange={handleChange}
-        className="peer h-full min-h-[100px] w-full resize-none rounded-[7px] border-2 border-gray-50 bg-transparent px-3 py-2.5 text-sm font-normal text-gray-500 outline outline-0 transition-all placeholder-shown:border-gray-50 placeholder-shown:border-t-gray-50 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 dark:border-gray-400 dark:placeholder-shown:border-gray-400 dark:focus:border-pink-500 dark:focus:border-t-transparent"
-        placeholder=" "
+        className='peer h-full min-h-[100px] w-full resize-none rounded-[7px] border-2 border-gray-50 bg-transparent px-3 py-2.5 text-sm font-normal text-gray-500 outline outline-0 transition-all placeholder-shown:border-gray-50 placeholder-shown:border-t-gray-50 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 dark:border-gray-400 dark:placeholder-shown:border-gray-400 dark:focus:border-pink-500 dark:focus:border-t-transparent'
+        placeholder=' '
       ></textarea>
       <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-white transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-transparent before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-transparent after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2  peer-focus:before:border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-pink-500">
         {value ? "" : placeholderText}
@@ -105,19 +106,19 @@ const Contact = ({ setSelectedPage }) => {
     <motion.section
       onViewportEnter={() => setSelectedPage(Pages.Contact)}
       variants={staggerContainer()}
-      initial="hidden"
-      whileInView="show"
+      initial='hidden'
+      whileInView='show'
       viewport={{ once: true, amount: 0.25 }}
       className={`${styles.padding} relative mx-auto max-w-7xl`}
     >
-      <span className="hash-span" id="contact">
+      <span className='hash-span' id='contact'>
         &nbsp;
       </span>
 
-      <div className="flex flex-col-reverse gap-10 overflow-hidden xl:mt-12 xl:flex-row">
+      <div className='flex flex-col-reverse gap-10 overflow-hidden xl:mt-12 xl:flex-row'>
         <motion.div
           variants={slideIn("left", "tween", 0.2, 1)}
-          className="flex-[0.75] rounded-2xl bg-blue-200 p-10 dark:bg-gray-700 dark:bg-opacity-40 md:p-[60px]"
+          className='flex-[0.75] rounded-2xl bg-blue-200 p-10 dark:bg-gray-700 dark:bg-opacity-40 md:p-[60px]'
         >
           <p className={styles.sectionSubText}>Get in touch</p>
           <h3 className={styles.sectionHeadText}>Contact.</h3>
@@ -125,7 +126,7 @@ const Contact = ({ setSelectedPage }) => {
           <form
             ref={formRef}
             onSubmit={handleSubmit}
-            className="mt-6 flex flex-col gap-4"
+            className='mt-6 flex flex-col gap-4'
           >
             <InputField
               type={"text"}
@@ -148,17 +149,15 @@ const Contact = ({ setSelectedPage }) => {
               placeholderText={"Message"}
             />
 
-            <div className="flex justify-end">
-              <div className="group relative">
-                <div
-                  className="absolute -inset-0.5 hidden rounded-lg bg-gradient-to-r from-pink-600 to-purple-600 opacity-50 blur transition duration-1000 ease-in-out group-hover:opacity-100 group-hover:duration-200 dark:block"
-                ></div>
+            <div className='flex justify-end'>
+              <div className='group relative'>
+                <div className='absolute -inset-0.5 hidden rounded-lg bg-gradient-to-r from-pink-600 to-purple-600 opacity-50 blur transition duration-1000 ease-in-out group-hover:opacity-100 group-hover:duration-200 dark:block'></div>
                 <button
-                  type="submit"
-                  className="group relative h-12 w-fit overflow-hidden rounded-lg bg-white pr-6 pl-7 text-lg shadow dark:bg-gray-800"
+                  type='submit'
+                  className='group relative h-12 w-fit overflow-hidden rounded-lg bg-white pr-6 pl-7 text-lg shadow dark:bg-gray-800'
                 >
-                  <div className="absolute inset-0 w-3 bg-blue-out transition-all duration-[250ms] ease-out group-hover:w-full dark:hidden"></div>
-                  <span class="relative text-black group-hover:text-white dark:text-white">
+                  <div className='absolute inset-0 w-3 bg-blue-out transition-all duration-[250ms] ease-out group-hover:w-full dark:hidden'></div>
+                  <span class='relative text-black group-hover:text-white dark:text-white'>
                     {loading ? "Sending..." : "Send"}
                   </span>
                 </button>
@@ -169,7 +168,7 @@ const Contact = ({ setSelectedPage }) => {
 
         <motion.div
           variants={slideIn("right", "tween", 0.2, 1)}
-          className="h-[350px] md:h-[550px] xl:h-auto xl:flex-1"
+          className='h-[350px] md:h-[550px] xl:h-auto xl:flex-1'
         >
           <EarthCanvas />
         </motion.div>
