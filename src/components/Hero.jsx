b24f49c782af6829b10a1806cc20c5977a18407b
jerryhuangyu <jerry0932619628@gmail.com>
2023-05-28 14:44:44 +0800

    prettier

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,25 +10,25 @@ const Hero = () => {
   const { t } = useTranslation();
 
   return (
-    <section className="relative mx-auto h-screen w-full">
+    <section className='relative mx-auto h-screen w-full'>
       <div
         className={`${styles.paddingX} absolute inset-0 top-[160px] mx-auto flex max-w-7xl flex-row items-start gap-4 sm:top-[190px] md:gap-10`}
       >
         {/* lefthand side line decorator */}
-        <div className="z-30 mt-5 flex flex-col items-center justify-center">
-          <div className="h-5 w-5 rounded-full bg-blue-out" />
-          <div className="blue-gradient h-40 w-1 sm:h-80"></div>
+        <div className='z-30 mt-5 flex flex-col items-center justify-center'>
+          <div className='h-5 w-5 rounded-full bg-blue-out' />
+          <div className='blue-gradient h-40 w-1 sm:h-80'></div>
         </div>
 
         {/* introduction */}
-        <div className="z-30 w-[600px] rounded-xl backdrop-blur-[4px] dark:bg-opacity-[0.01]">
+        <div className='z-30 w-[600px] rounded-xl backdrop-blur-[4px] dark:bg-opacity-[0.01]'>
           <HackerText />
           <p className={`${styles.heroSubText} mt-4`}>
             {t("hero.intro1")}
             <br />
             {t("hero.intro2")}&nbsp;
             <Typed
-              className="font-bold text-blue-800 dark:text-primary"
+              className='font-bold text-blue-800 dark:text-primary'
               strings={["Web.", "MR.", "ROS."]}
               typeSpeed={180}
               backSpeed={110}
@@ -41,9 +41,9 @@ const Hero = () => {
       <HeroModelCanvas />
 
       {/* scrolling button */}
-      <div className="absolute bottom-20 z-30 flex w-full items-center justify-center sm:bottom-32">
-        <a href="#about" aria-label="Read more">
-          <div className="flex h-[64px] w-[35px] items-start justify-center rounded-3xl border-4 border-secondary p-2">
+      <div className='absolute bottom-20 z-30 flex w-full items-center justify-center sm:bottom-32'>
+        <a href='#about' aria-label='Read more'>
+          <div className='flex h-[64px] w-[35px] items-start justify-center rounded-3xl border-4 border-secondary p-2'>
             <motion.div
               animate={{
                 y: [0, 24, 0],
@@ -53,7 +53,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className="mb-1 h-3 w-3 rounded-full bg-secondary"
+              className='mb-1 h-3 w-3 rounded-full bg-secondary'
             />
           </div>
         </a>
